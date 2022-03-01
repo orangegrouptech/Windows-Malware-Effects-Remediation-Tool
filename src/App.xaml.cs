c7d859e3f71d348aa59263415758e9e300b3d37e
Orange Group Tech <orangemanagementcorpn@gmail.com>
2022-03-01 12:00:01 +0800

    3.0

---
+++ src/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Windows_Malware_Effects_Remediation_Tool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+}
